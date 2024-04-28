0cf422f7bb97fd8362c56bebc7637cdb69aa2cfd
Techit Sumo Thawiang <68545084+techittttt@users.noreply.github.com>
2024-04-28 23:43:22 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: Readonly <{ children: React.Rea
 return (
     <html lang="en" className={colorScheme}>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://kit.fontawesome.com/fad05709e1.js" crossOrigin="anonymous" async></script>
       </head>
       <body className={inter.className}>
