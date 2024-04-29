0e1bc06cd60c9803850e7fb8d442c2954fc4f10b
techittttt <peesumo2018@gmail.com>
2024-04-29 08:55:23 +0700

    Removed aurora background

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import {Button, Image} from "@nextui-org/react";
-import { AuroraBackground } from "@/app/_Components/AuroraBackground";
 
 const AboutPage = () => {
   const handleButtonClick = (link: string) => {
@@ -12,32 +11,30 @@ const AboutPage = () => {
     }
   };
   return (
-    <AuroraBackground>
-      <motion.div
-      initial={{ opacity: 0.0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: "easeInOut",
-      }}
-      className="w-full justify-center flex flex-col items-center">
-        <section className="min-h-screen w-full px-6 flex flex-col max-w-[1024px] justify-evenly gap-1 items-start">
-          <section className="flex flex-col gap-4">
-            <section>
-              <Image alt="email icon" height={96} width={96} src="https://images.techit.win/papirus-icon-theme-master/Papirus/22x22/apps/user-info.svg"></Image>
-            </section>
-            <section className="gap-2 flex flex-col">
-              <h1 className="text-5xl md:text-5xl font-black dark:text-white text-black">About Me</h1>
-            </section>      
-          </section>
+    <motion.div
+    initial={{ opacity: 0.0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}
+    className="w-full justify-center flex flex-col items-center">
+      <section className="min-h-screen w-full px-6 flex flex-col max-w-[1024px] justify-evenly gap-1 items-start">
+        <section className="flex flex-col gap-4">
           <section>
-            <p className="text-2xl md:text-2xl dark:text-white text-black"><strong>Name:</strong> Techit Thawiang</p>
-            <p className="text-2xl md:text-2xl dark:text-white text-black"><strong>Interested in:</strong> Network, Computers, Technologoies</p>
+            <Image alt="email icon" height={96} width={96} src="https://images.techit.win/papirus-icon-theme-master/Papirus/22x22/apps/user-info.svg"></Image>
           </section>
-        </section>        
-      </motion.div>
-    </AuroraBackground>
+          <section className="gap-2 flex flex-col">
+            <h1 className="text-5xl md:text-5xl font-black dark:text-white text-black">About Me</h1>
+          </section>      
+        </section>
+        <section>
+          <p className="text-2xl md:text-2xl dark:text-white text-black"><strong>Name:</strong> Techit Thawiang</p>
+          <p className="text-2xl md:text-2xl dark:text-white text-black"><strong>Interested in:</strong> Network, Computers, Technologoies</p>
+        </section>
+      </section>        
+    </motion.div>
   );
 };
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import {Button, Image, Divider} from "@nextui-org/react";
-import { AuroraBackground } from "@/app/_Components/AuroraBackground";
 
 const HomePage = () => {
   const handleButtonClick = (link: string) => {
@@ -12,33 +11,31 @@ const HomePage = () => {
     }
   };
   return (
-    <AuroraBackground>
-      <motion.div
-      initial={{ opacity: 0.0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: "easeInOut",
-      }}
-      className="w-full justify-center flex flex-col items-center">
-        <section className="min-h-screen w-full px-6 flex flex-col max-w-[1024px] justify-evenly gap-1 items-start">
-          <section className="flex flex-col gap-4">
-            <section>
-              <Image alt="email icon" height={96} width={96} src="https://images.techit.win/papirus-icon-theme-master/Papirus/22x22/apps/email.svg"></Image>
-            </section>
-            <section className="gap-2 flex flex-col">
-              <h1 className="text-5xl md:text-5xl font-black dark:text-white text-black">Contact Me</h1>
-            </section>      
+    <motion.div
+    initial={{ opacity: 0.0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}
+    className="w-full justify-center flex flex-col items-center">
+      <section className="min-h-screen w-full px-6 flex flex-col max-w-[1024px] justify-evenly gap-1 items-start">
+        <section className="flex flex-col gap-4">
+          <section>
+            <Image alt="email icon" height={96} width={96} src="https://images.techit.win/papirus-icon-theme-master/Papirus/22x22/apps/email.svg"></Image>
           </section>
-          <section className="gap-2 flex flex-row">
-            <Button onClick={() => handleButtonClick("mailto://techit@dailitation.xyz")} size="md" color="primary" variant="shadow">E-mail</Button>
-            <Button onClick={() => handleButtonClick("https://twitter.com/techitttttt")} size="md">X (Twitter)</Button>
-            <Button onClick={() => handleButtonClick("https://github.com/techittttt")} size="md">GitHub</Button>
-          </section>
-        </section>        
-      </motion.div>
-    </AuroraBackground>
+          <section className="gap-2 flex flex-col">
+            <h1 className="text-5xl md:text-5xl font-black dark:text-white text-black">Contact Me</h1>
+          </section>      
+        </section>
+        <section className="gap-2 flex flex-row">
+          <Button onClick={() => handleButtonClick("mailto://techit@dailitation.xyz")} size="md" color="primary" variant="shadow">E-mail</Button>
+          <Button onClick={() => handleButtonClick("https://twitter.com/techitttttt")} size="md">X (Twitter)</Button>
+          <Button onClick={() => handleButtonClick("https://github.com/techittttt")} size="md">GitHub</Button>
+        </section>
+      </section>        
+    </motion.div>
   )
 };
 
