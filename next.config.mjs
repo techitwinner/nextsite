8fb275db9b50b382e352f49be49c6344cd09b532
techittttt <peesumo2018@gmail.com>
2024-05-09 11:43:41 +0700

    ski

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.extensions.push('.mjs');
+        return config;
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 export default nextConfig;
