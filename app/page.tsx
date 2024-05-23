038479cda3ed43693263ea3aeb4c168928a76a46
Techit Sumo Thawiang <peesumo2018@gmail.com>
2024-05-23 19:23:03 +0700

    Fixed a lot of stuff!

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,64 @@
-import {Button, Divider} from "@nextui-org/react";
-import React from "react";
+"use client";
+
+import {Button, Divider, Tooltip} from "@nextui-org/react";
+import React, {useEffect, useRef, useState} from "react";
 import {Image} from "@nextui-org/react";
 
+const socialItems = () => [
+    {"label": "GitHub", "website": "https://github.com/", "username": "techitwinner", "icon": "github-logo"},
+    {"label": "X / Twitter", "website": "https://x.com/", "username": "techitwinner", "icon": "x-logo"},
+    {"label": "Facebook", "website": "https://facebook.com/", "username": "techitwinner", "icon": "facebook-logo"}
+];
+
+const handleButtonClick = (link: string) => {
+    if (typeof window !== 'undefined') {
+        window.location.href = link;
+    }
+};
+
 const Home = () => {
-    const pfpSize = "150px"
+    const pfpSize = "200px"
     return (
-        <div className="min-h-screen w-full flex flex-col">
-            <div className="h-screen flex flex-col justify-center items-start gap-6">
-                <div className="flex flex-col sm:flex-row justify-center items-start sm:items-center gap-8">
-                    <section>
-                        <Image src="https://images.techit.win/custom/PFP/realtechit.jpg" height={pfpSize}
-                               width={pfpSize}/>
+        <div className="h-screen flex flex-col justify-evenly items-start gap-6">
+            <div className="flex flex-col justify-center items-start gap-8">
+                <section>
+                    <Image src="https://images.techit.win/custom/PFP/realtechit.jpg" height={pfpSize}
+                           width={pfpSize}
+                           radius="sm"/>
+                </section>
+                <section className="gap-2 flex-col flex">
+                    <p className="font-bold text-5xl">Techit Thawiang</p>
+                    <section className="flex flex-row gap-4 w-full">
+                        <p className="inline-flex items-center gap-1"><i className="ph-fill ph-map-pin"></i>Thailand</p>
+                        <p className="inline-flex items-center gap-1"><i className="ph-fill ph-cake"></i>Jan, 05</p>
+                        <p className="inline-flex items-center gap-1"><i className="ph-fill ph-bug"></i>Student,
+                            Developer</p>
                     </section>
-                    <section className="gap-2 flex-col flex">
-                        <p className="font-bold text-5xl">Techit Thawiang</p>
-                        <section className="flex flex-row gap-4 w-full">
-                            <p className="inline-flex items-center gap-1"><i className="ph-fill ph-map-pin"></i>Thailand
-                            </p>
-                            <p className="inline-flex items-center gap-1"><i className="ph-fill ph-cake"></i>Jan, 05</p>
-                            <p className="inline-flex items-center gap-1"><i className="ph-fill ph-bug"></i>Developer
-                            </p>
-                        </section>
-                    </section>
-                </div>
-                <div>
-                    <section>
-                        <Button size="lg">About Me</Button>
-                    </section>
-                </div>
+                </section>
+                <section className="gap-2 flex">
+                    {socialItems().map((item, index) => (
+                        <Tooltip key={index} showArrow={true} content={item.label} placement="bottom">
+                            <Button
+                                radius="full"
+                                isIconOnly
+                                variant="ghost"
+                                onClick={() => handleButtonClick(item.website + item.username)}
+                            >
+                                <p className={`ph ph-${item.icon} text-[1.25rem]`}></p>
+                            </Button>
+                        </Tooltip>
+
+                    ))}
+                </section>
             </div>
-        </div>
-    )
+            <div className="flex flex-col gap-2">
+
+                <section className="gap-2 flex">
+                    <Button radius="sm" color="primary" onClick={() => handleButtonClick("/about")}>About Me</Button>
+                    <Button radius="sm" variant="faded" onClick={() => handleButtonClick("/blog")}>Blog</Button>
+                </section>
+            </div>
+        </div>)
 }
 
 export default Home
