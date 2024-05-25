a6cfcfebde5d070da8d55d4e2d52f4e8d5d0d0a4
Techit Sumo Thawiang <peesumo2018@gmail.com>
2024-05-25 15:15:19 +0700

    Improved footer and navigator section

--- app/_MainComponents.tsx
+++ app/_MainComponents.tsx
@@ -2,7 +2,7 @@
 
 import React, {useState} from "react";
 import {Inter_Tight} from "next/font/google";
-import {Button, Link, Tooltip} from "@nextui-org/react";
+import {Button, Link, Tooltip, Image} from "@nextui-org/react";
 import {AnimatePresence, motion} from "framer-motion";
 import {usePathname} from "next/navigation";
 
@@ -15,6 +15,8 @@ const handleButtonClick = (link: string) => {
 };
 
 const day = new Date();
+
+const logoUrl = "https://images.techit.win/logos/techit.win/TechitDotWinLogo.png"
 
 const Navigator = () => {
     const currentPath = usePathname();
@@ -97,23 +99,25 @@ const Navigator = () => {
             <AnimatePresence>
                 {isOpen && (
                     <motion.div
-                        className={`${isOpen ? 'fixed' : 'hidden'} z-[9998] flex flex-col w-screen h-screen backdrop-blur bg-white dark:bg-black bg-opacity-70 dark:bg-opacity-70 gap-2 p-4 pt-20`}
+                        className={`${isOpen ? 'fixed' : 'hidden'} z-[9998] flex w-screen h-screen backdrop-blur bg-white dark:bg-black bg-opacity-70 dark:bg-opacity-70 gap-2 pt-20 justify-center`}
                         initial="closed"
                         animate="open"
                         exit="closed"
                         variants={menuVariants}
                         transition={menuTransition}
                     >
-                        {navigatorContents().map((nav, index) => (
-                            <Link key={index}
-                                    href={nav.url}
-                                    className="text-5xl font-bold"
-                                    underline={currentPath === nav.url ? "always" : "hover"}
-                                    color={currentPath === nav.url ? "primary" : "foreground"}
-                            >
-                                {nav.label}
-                            </Link>
-                        ))}
+                        <nav className="flex flex-col max-w-[64rem] w-full gap-2 px-4 sm:px-8">
+                            {navigatorContents().map((nav, index) => (
+                                <Link key={index}
+                                      href={nav.url}
+                                      className="text-5xl font-bold gap-2"
+                                      color={currentPath === nav.url ? "primary" : "foreground"}
+                                >
+                                    {nav.label}
+                                </Link>
+                            ))}
+                        </nav>
+
                     </motion.div>
                 )}
             </AnimatePresence>
@@ -122,15 +126,83 @@ const Navigator = () => {
 }
 
 const Footer = () => {
+    const footerLogoSize = "96px"
+    const currentPath = usePathname();
+
+    const legalSection = () => [
+        {"label": "Imprint", "url": "/imprint"},
+        {"label": "Attributions", "url": "/attributions"},
+    ];
+
+    const socialSection = () => [
+        {"label": "GitHub", "website": "https://github.com/", "username": "techitwinner", "icon": "github-logo"},
+        {"label": "X / Twitter", "website": "https://x.com/", "username": "techitwinner", "icon": "x-logo"},
+        {"label": "Facebook", "website": "https://facebook.com/", "username": "techitwinner", "icon": "facebook-logo"},
+        {"label": "LinkedIn", "website": "https://linkedin.com/", "username": "in/techit-thawiang-02b56b27a", "icon": "linkedin-logo"}
+    ];
+
     return (
         <React.Fragment>
-            <div className="w-full items-center justify-center">
+            <div className="flex flex-col gap-6 w-full items-center justify-center border-t-2 border-black border-opacity-15 dark:border-white dark:border-opacity-15 py-10">
                 <section
-                    className="py-2 flex flex-row h-full justify-center items-center border-t-2 border-black border-opacity-15 dark:border-white dark:border-opacity-15">
-                    <nav className="flex flex-col max-w-[64rem] px-4 sm:px-8 w-full justify-center items-center">
-                        <p className="text-[12px]">© 2023-{day.getFullYear()} Techit Thawiang. All rights reserved.</p>
-                        <p className="text-[12px]">Any logos or trademarks appearing on this site are the property of
-                            their respective owners.</p>
+                    className="flex flex-row h-full justify-center items-center max-w-[64rem] w-full">
+                    <nav className="flex flex-row max-w-[64rem] px-4 sm:px-8 w-full justify-center items-stretch">
+                        <section className="items-start flex flex-col w-full gap-4">
+                            <Image alt=""
+                                   src={logoUrl}
+                                   width={footerLogoSize}
+                                   radius="sm"
+                            />
+                        </section>
+                    </nav>
+                </section>
+                <section
+                    className="flex flex-row h-full justify-center items-center max-w-[64rem] w-full">
+                    <nav className="flex flex-col sm:flex-row max-w-[64rem] px-4 sm:px-8 w-full justify-center items-stretch gap-2">
+                        <section className="items-start flex flex-col w-full sm:w-[135%]">
+                            <p className={`${interTight.className} text-2xl font-bold select-none cursor-default`}>Techit
+                                Thawiang</p>
+                            <p>© 2023-{day.getFullYear()}, All rights reserved.</p>
+                        </section>
+                        <div className="grid grid-cols-2 sm:flex sm:flex-row w-full">
+                            <section className="items-start flex flex-col w-full">
+                                <section className="flex flex-row justify-start gap-1">
+                                    <i className="text-2xl ph-bold ph-asterisk-simple self-start mt-1"></i>
+                                    <section className="flex flex-col">
+                                        <p className={`${interTight.className} flex flex-col md:flex-row text-2xl font-bold select-none cursor-default`}>
+                                            Legal</p>
+                                        {legalSection().map((legal, index) => (
+                                            <Link key={index}
+                                                  href={legal.url}
+                                                  underline={`${currentPath === legal.url ? "always" : "none"}`}
+                                                  color={`${currentPath === legal.url ? "primary" : "foreground"}`}
+                                                  className="select-none cursor-pointer"
+                                            >
+                                                {legal.label}
+                                            </Link>
+                                        ))}
+                                    </section>
+                                </section>
+                            </section>
+                            <section className="items-start flex flex-col w-full">
+                                <section className="flex flex-row justify-start gap-1">
+                                    <i className="text-2xl ph-bold ph-asterisk-simple self-start mt-1"></i>
+                                    <section className="flex flex-col">
+                                        <p className={`${interTight.className} flex flex-col md:flex-row text-2xl font-bold select-none cursor-default`}>
+                                            Social</p>
+                                        {socialSection().map((social, index) => (
+                                            <Link key={index}
+                                                  href={`${social.website}${social.username}`}
+                                                  color="foreground"
+                                                  className="select-none cursor-pointer"
+                                            >
+                                                {social.label}
+                                            </Link>
+                                        ))}
+                                    </section>
+                                </section>
+                            </section>
+                        </div>
                     </nav>
                 </section>
             </div>
