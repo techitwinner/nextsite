fa18ecd34792159d94bd133363de3245223eebe8
Techit Sumo Thawiang <peesumo2018@gmail.com>
2024-05-29 22:03:12 +0700

    bla

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-export default async function () {
+export default function () {
     return (
         <div className="min-h-screen py-24 flex flex-col gap-16">
             <h1 className="text-5xl font-bold">About Me!</h1>
