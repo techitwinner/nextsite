072002c1b188e1bbbf17adea5f87d86439568164
Techit Sumo Thawiang <peesumo2018@gmail.com>
2024-05-29 22:04:36 +0700

    bla

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function About () {
     return (
         <div className="min-h-screen py-24 flex flex-col gap-16">
             <h1 className="text-5xl font-bold">About Me!</h1>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function Projects () {
     return (
         <div className="min-h-screen py-24 flex flex-col gap-16">
             <h1 className="text-5xl font-bold">My Projects</h1>
